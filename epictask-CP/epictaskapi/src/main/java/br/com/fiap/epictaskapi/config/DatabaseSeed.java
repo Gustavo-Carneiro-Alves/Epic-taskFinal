921481e0f23c8bbc0f9032c8fde4ea98191dbe9d
Gustavo-Carneiro-Alves <80040530+Gustavo-Carneiro-Alves@users.noreply.github.com>
2022-09-14 20:13:28 -0300

    Add files via upload

---
+++ epictask-CP/epictaskapi/src/main/java/br/com/fiap/epictaskapi/config/DatabaseSeed.java
@@ -0,0 +1,45 @@
+package br.com.fiap.epictaskapi.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import br.com.fiap.epictaskapi.model.User;
+import br.com.fiap.epictaskapi.repository.UserRepository;
+
+@Configuration
+@Profile("dev")
+public class DatabaseSeed implements CommandLineRunner {
+
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.saveAll(List.of(
+            new User()
+                .name("João")
+                .email("joao@fiap.com.br")
+                .password(passwordEncoder.encode("123")),
+            new User()
+                .name("Carlos")
+                .email("carlos@fiap.com.br")
+                .password(passwordEncoder.encode("456")),
+            new User()
+                .name("Roberto")
+                .email("roberto@fiap.com.br")
+                .password(passwordEncoder.encode("789"))
+        ));
+
+    }
+    
+}
